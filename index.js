ae5acae0f469e94a05cf32ebfdb421c8248009db
Puune <43564655+Puune@users.noreply.github.com>
2019-11-11 14:59:41 +0200

    uibuild

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(cors());
-//app.use(express.static('build'));
+app.use(express.static('build'));
 
 morgan.token('data', function(req, res) {
     return (JSON.stringify(req.body))
